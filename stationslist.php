50452b3433a3bd6c83d82356eccd64678b82c345
Marbyallen <m.agbunagmarby@gmail.com>
2021-09-04 15:38:27 +0800

    Update stationslist.php

--- stationslist.php
+++ stationslist.php
@@ -168,6 +168,24 @@
                                     echo "</table>";
                                     mysqli_close($mysqli);
                                     }
+                                    while($row = mysqli_fetch_array($result))
+                                    {
+                                              echo "<tr>";
+                                              echo "<td>" . $row['QRcode'] . "</td>";
+                                              echo "<td>" . $row['firstname'] . "</td>";
+                                              echo "<td>" . $row['lastname'] . "</td>";
+                                              echo "<td>" . $row['contactno'] . "</td>";
+                                              echo "<td>" . $row['email'] . "</td>";
+                                              echo "<td>" . $row['station'] . "</td>";
+                                              echo "<td>" . $row['facialimg'] . "</td>";
+                                              echo "<td>" . $row['date'] . "</td>";
+                                              echo "<td>" . $row['time'] . "</td>";
+                                              echo "<td>" . $row['bodyheat_temp'] . "</td>";
+                                              echo "</tr>";
+                                    }
+                                    echo "</table>";
+                                    mysqli_close($mysqli);
+                                    }
                         }
                         ?>
           </main>
