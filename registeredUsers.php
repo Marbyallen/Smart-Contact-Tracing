5a8f0effae3e6e75bfd8163058d08dd75f61d4e1
Marbyallen <m.agbunagmarby@gmail.com>
2021-09-04 14:48:28 +0800

    Update registeredUsers.php

--- registeredUsers.php
+++ registeredUsers.php
@@ -59,23 +59,23 @@
                               </tr>
                               <!-- search first name -->
                               <?php
-                              while($row = mysqli_fetch_array($result))
-                              {
-                              echo "<tr>";
-                              echo "<td>" . $row['QRcode'] . "</td>";
-                              echo "<td>" . $row['firstname'] . "</td>";
-                              echo "<td>" . $row['lastname'] . "</td>";
-                              echo "<td>" . $row['contactno'] . "</td>";
-                              echo "<td>" . $row['email'] . "</td>";
-                              echo "<td>" . $row['address'] . "</td>";
-                              echo "</tr>";
-                              }
-                            echo "</table>";
-                            mysqli_close($mysqli);
                               if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                         // collect value of input field
                                         $Sfname = $_REQUEST['Sfname'];
                                         $Slname = $_REQUEST['Slname'];
+                                        while($row = mysqli_fetch_array($result))
+                                                                      {
+                                                                      echo "<tr>";
+                                                                      echo "<td>" . $row['QRcode'] . "</td>";
+                                                                      echo "<td>" . $row['firstname'] . "</td>";
+                                                                      echo "<td>" . $row['lastname'] . "</td>";
+                                                                      echo "<td>" . $row['contactno'] . "</td>";
+                                                                      echo "<td>" . $row['email'] . "</td>";
+                                                                      echo "<td>" . $row['address'] . "</td>";
+                                                                      echo "</tr>";
+                                                                      }
+                                                  echo "</table>";
+                                                  mysqli_close($mysqli);
                                         if (!empty($Sfname)) {
                                                   $result = mysqli_query($mysqli," SELECT  * FROM customers_table WHERE firstname LIKE '".$Sfname."' ");
                                                             while($row = mysqli_fetch_array($result))
