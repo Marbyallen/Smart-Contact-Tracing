9a391111090cdc154323b3802598fc871a061e19
marbyagbunag <m.agbunagmarby@gmail.com>
2021-09-19 13:56:56 +0800

    Update index.php

--- admin_folder/index.php
+++ admin_folder/index.php
@@ -5,7 +5,6 @@
         <meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width"/>
     <link rel="stylesheet" type="text/css" href="https://Marbyallen.github.io/assets/mymain1.css">
-        <link rel="stylesheet" type="text/css" href="https://github.com/Marbyallen/Smart-Contact-Tracing/blob/f903fc9c4aa538ac7199fa5e6438551126321566/assets/mymain1.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
           </head>
           <body>
