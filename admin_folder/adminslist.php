871012fec49836fb74ce6705fcbe9d5969201767
marbyagbunag <m.agbunagmarby@gmail.com>
2021-09-19 15:26:37 +0800

    css update

--- admin_folder/adminslist.php
+++ admin_folder/adminslist.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-US">
           <head> 
-                    <link rel="stylesheet" href="/assets/mymain1.css">
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-                    <title>Admins list</title>
+            <link rel="stylesheet" type="text/css" href="\assets_admin\mymain1.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>Admins list</title>
           </head>
           <body>
                    
