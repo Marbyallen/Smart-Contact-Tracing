5854bbbbdf495cbe988a5a0f0c238efc14672c70
marbyagbunag <m.agbunagmarby@gmail.com>
2022-01-13 17:07:51 +0800

    Update stationslist.php

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -125,9 +125,10 @@
                                   $Sfname = $_REQUEST['Sfname'];
                                   $Slname = $_REQUEST['Slname'];
                                   $QRcode = $_REQUEST['QRcode'];
-                                  $date1 = $_REQUEST['date1'];
-                                  $date2 = $_REQUEST['date2'];
-                                  
+                                //   $date1 = $_REQUEST['date1'];
+                                //   $date2 = $_REQUEST['date2'];
+                                  $date1 = date("Y-m-d", strtotime($_POST['date1']));
+                                  $date2 = date("Y-m-d", strtotime($_POST['date2']));
                                   if (!empty($Sfname)) {
                                             $result = mysqli_query($mysqli," SELECT  * FROM usersAndStations_table WHERE firstname LIKE '".$Sfname."' ");
                                                       while($row = mysqli_fetch_array($result))
@@ -188,8 +189,8 @@
                                                         }
                                                         echo "</table>";
                                                         mysqli_close($mysqli);
-                                  } elseif(!empty($date1) and !empty($date2)){
-                                    $result = mysqli_query($mysqli,"SELECT * FROM usersAndStations_table WHERE 'date' BETWEEN '".$date1."' AND '".$date2."' ");
+                                  } elseif(!empty($date1) && !empty($date2)){
+                                    $result = mysqli_query($mysqli, "SELECT * FROM `usersAndStations_table` WHERE `date` BETWEEN '$date1' AND '$date2'") or die(mysqli_error());
                                     while($row = mysqli_fetch_array($result))
                                     {
                                             echo "<tr>";
