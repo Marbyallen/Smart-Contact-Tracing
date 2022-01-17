d33a85605838052c0a90acb9a380ca3a9b01b837
marbyagbunag <m.agbunagmarby@gmail.com>
2022-01-17 16:51:41 +0800

    Update registeredUsers.php

--- admin_folder/registeredUsers.php
+++ admin_folder/registeredUsers.php
@@ -25,14 +25,14 @@
                                             <div class="row"><br>
                                                   <div class="col-md-4">
                                                       <input id="fname" name="Sfname" type="text" placeholder="First Name" class="form-control input-md"><br>
+                                                      <input id="lname" name="Slname" type="text" placeholder="Last Name" class="form-control input-md"><br>
                                                       <button type="submit" class="btn btn-primary" >Search</button>      
                                                   </div>
                                         
                                         
-                                              <div class="col-md-4">
-                                                  <input id="lname" name="Slname" type="text" placeholder="Last Name" class="form-control input-md"><br>
+                                              <!-- <div class="col-md-4">
                                                   
-                                              </div>
+                                              </div> -->
                                             </div>
                                         </form><br>
                     <?php
