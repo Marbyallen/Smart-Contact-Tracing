b8c1e84b4e2cc1cdd7b4273976e35e72f7c1d1d8
marbyagbunag <m.agbunagmarby@gmail.com>
2022-03-20 11:50:13 +0800

    testing fix in db_connect

--- admin_folder/db_connect.php
+++ admin_folder/db_connect.php
@@ -2,11 +2,22 @@
 #server executed code
 #this connects php to mySQL
 // 4 variable to connect to database
-$host = "localhost";
-$username = "u923368226_tracingconnect";
-$user_pass = "Mainsct21!";
-$database_in_use = "u923368226_contactTracing";
+// $host = "localhost";
+// $username = "u923368226_tracingconnect";
+// $user_pass = "Mainsct21!";
+// $database_in_use = "u923368226_contactTracing";
 
 // create database connection instance
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+// $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+
+$dbhost = "localhost";
+$dbuser = "u923368226_tracingconnect";
+$dbpass = "Mainsct21";
+$dbname = "u923368226_contactTracing";
+
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+
+	die("failed to connect!");
+}
 ?>
