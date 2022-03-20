70fffa8fd0b5825496cf744dcfca5fcf27ef1333
marbyagbunag <m.agbunagmarby@gmail.com>
2022-03-20 11:56:49 +0800

    Update db_connect.php

--- admin_folder/db_connect.php
+++ admin_folder/db_connect.php
@@ -2,18 +2,21 @@
 #server executed code
 #this connects php to mySQL
 // 4 variable to connect to database
-$host = "localhost";
-$username = "u923368226_tracingconnect";
-$user_pass = "Mainsct21!";
-$database_in_use = "u923368226_contactTracing";
-
-create database connection instance
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-
+// $host = "localhost";
+// $username = "u923368226_tracingconnect";
+// $user_pass = "Mainsct21!";
+// $database_in_use = "u923368226_contactTracing";
+//---
 $dbhost = "localhost";
 $dbuser = "u923368226_tracingconnect";
 $dbpass = "Mainsct21";
 $dbname = "u923368226_contactTracing";
+
+
+create database connection instance
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+
+
 
 // if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 // {
