50345fd130d19e02176c5690665cb92d3319b5f8
marbyagbunag <m.agbunagmarby@gmail.com>
2022-03-20 12:39:01 +0800

    Update stationB.php

--- admin_folder/stationB.php
+++ admin_folder/stationB.php
@@ -15,7 +15,7 @@
                               <li> <a href="registeredUsers.html">Registered Users</a></li>
                               <li> <a href="adminslist.php">Admins List</a></li>
                               <li><a href="stationslist.php">Stations List</a></li>
-                              <li style="float: right;"> <a href="index.html">Logout</a></li>
+                              <li style="float: right;"> <a href="index.php">Logout</a></li>
                     </ul>
                    </div>
                     <main>
