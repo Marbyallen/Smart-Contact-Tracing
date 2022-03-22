36bf2c23fe6539204c6321ebc1465a644f745f7e
marbyagbunag <m.agbunagmarby@gmail.com>
2022-03-22 13:37:07 +0800

    Update adminlogin.php

--- admin_folder/adminlogin.php
+++ admin_folder/adminlogin.php
@@ -61,7 +61,7 @@ session_start();
             <main>
 
               <h2>Login as Admin</h2>
-              <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+              <form method="post">
 
                 <div class="form-group">
                 <label>Username</label>
