693fccb9d4dfc4b280c10a759418aa3a48ac074d
marbyagbunag <m.agbunagmarby@gmail.com>
2022-03-22 12:54:17 +0800

    Update functions.php

--- admin_folder/functions.php
+++ admin_folder/functions.php
@@ -1,13 +1,14 @@
 <?php
 
 function check_login($con)
+{
 // QRcode will be used as a user id 
     if(isset($_SESSION['QRcode']))
     {
         $id = $_SESSION['QRcode'];
         $query = "SELECT * FROM Allusers_table WHERE QRcode = '$id' limit 1";
 
-        $result = mysqli_query($con,$query);
+        $result = mysqli_query($con, $query);
         if($result && mysqli_num_rows($result) > 0)
         {
             $user_data = mysqli_fetch_aassoc($result);
@@ -18,4 +19,6 @@ function check_login($con)
     //redirect to login
     header("Location: adminlogin.php");
     die;
+}
 ?>
+
