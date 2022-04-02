59bbcac6f7be461d1c041cab0e7ca313ecb431a0
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-02 19:32:07 +0800

    Update functions.php

--- admin_folder/functions.php
+++ admin_folder/functions.php
@@ -1,10 +1,8 @@
 <?php
 function check_login($con)
 {
-
 	if(isset($_SESSION['QRcode']))
 	{
-
 		$id = $_SESSION['QRcode'];
 		$query = "SELECT * FROM Allusers_table WHERE QRcode LIKE  '".$id."' LIMIT 1";
 
@@ -12,7 +10,11 @@ function check_login($con)
 		if($result && mysqli_num_rows($result) > 0)
 		{
 
-			$user_data = mysqli_fetch_assoc($result);
+            $result = mysqli_query($con," SELECT  * FROM Allusers_table WHERE username LIKE '".$userName."' LIMIT 1");
+            $row = mysqli_fetch_array($result);
+            $user_data = mysqli_fetch_assoc($row);
+
+			// $user_data = mysqli_fetch_assoc($result);
 			return $user_data;
 		}
 	} else {
