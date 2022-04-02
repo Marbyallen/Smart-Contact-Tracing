ef364b6553831e4bf89d06bcf538123fe256880c
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-02 18:56:36 +0800

    added php for login verification

--- admin_folder/registeredUsers.php
+++ admin_folder/registeredUsers.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+	include('db_connect.php');
+	include('functions.php');
+
+	$user_data = check_login($con);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en-US">
           <head> 
