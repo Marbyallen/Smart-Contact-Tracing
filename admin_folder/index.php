9f7cbba6cdea6068fc6ed05c6e6580d38c1f4297
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-02 19:02:15 +0800

    logout out added

--- admin_folder/index.php
+++ admin_folder/index.php
@@ -25,7 +25,7 @@ session_start();
                               <li> <a href="registeredUsers.php">Users List</a></li>
                               <li> <a href="adminslist.php">Admins List</a></li>
                               <li><a href="stationslist.php">Stations List</a></li>
-                              <li style="float: right"> <a href="adminlogin.php">Logout</a></li>
+                              <li style="float: right"> <a href="logout.php">Logout</a></li>
                     </ul>
                    </div>
                    <main>
--- admin_folder/stationB.php
+++ admin_folder/stationB.php
@@ -20,11 +20,11 @@ session_start();
           <body>
                    <div class="sidenav">
                     <ul class="first">
-                              <li> <a href="hadmin.html">Home</a></li>
+                              <li> <a href="hadmin.php">Home</a></li>
                               <li> <a href="registeredUsers.html">Registered Users</a></li>
                               <li> <a href="adminslist.php">Admins List</a></li>
                               <li><a href="stationslist.php">Stations List</a></li>
-                              <li style="float: right;"> <a href="index.php">Logout</a></li>
+                              <li style="float: right;"> <a href="logout.php">Logout</a></li>
                     </ul>
                    </div>
                     <main>
