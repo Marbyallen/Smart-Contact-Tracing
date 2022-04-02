0160c84edd9e1dfe86679d36472f1d9046af3dd7
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-02 19:18:42 +0800

    Update index.php

--- admin_folder/index.php
+++ admin_folder/index.php
@@ -30,7 +30,7 @@ session_start();
                    </div>
                    <main>
                     <h1>Welcome to Head Admin Page</h1>
-                    Hello, <?php echo $_SESSION['username']; ?>
+                    Hello, <?php echo $user_data['username']; ?>
                    </main>
           </body>
 </html>
