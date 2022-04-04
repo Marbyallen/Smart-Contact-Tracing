10a54b98fbaba56c84ba2f41bb86a1fb727a2a65
hiriyoshi <81507450+hiriyoshi@users.noreply.github.com>
2022-04-04 19:42:42 +0800

    index

--- USERS_FOLDER/code/index.php
+++ USERS_FOLDER/code/index.php
@@ -1,45 +1,40 @@
-<?php
-	session_start();
-
-	if (!isset($_SESSION['username'])) {
-		$_SESSION['msg'] = "You must log in first";
-		header('location: login.php');
-	}
-	if (isset($_GET['logout'])) {
-		session_destroy();
-		unset($_SESSION['username']);
-		header("location: login.php");
-	}
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="index.css">
+    <title>THESIS</title>
 </head>
+<header>
+<nav class="nav">
+    <div class="logo"><a href="index.html">CONTACT <span>TRACING</span></a></div>
+    <ul class="nav_link">
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>
+    <div class="nav_button">
+        <li><button class="Signup"><a href="register.php">Sign Up</a></button></li>
+        <li><button class="Login"><a href="login.php">Log In</a></button></li>
+    </div>
+</nav>
+</header>
 <body>
-
-<div class="header">
-	<h2>Home Page</h2>
-</div>
-<div class="content">
-	<?php if (isset($_SESSION['success'])) : ?>
-		<div class="error success">
-			<h3>
-				<?php 
-					echo $_SESSION['success'];
-					unset($_SESSION['success']);
-				?>
-			</h3>
-		</div>
-	<?php endif ?>
-
-	<?php if (isset($_SESSION['username'])) : ?>
-		<p>Welcome <strong><?php echo $_SESSION['username']; ?><strong></p>
-		<p> <a href="index.php?logout='1'" style="color: red">logout</a></p>
-	<?php endif ?>
-</div>
-
+      <div class="wrapper">
+        <div class="background_message">
+            <section><p>COVID-19</p></section>
+            <section><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore, harum, iure incidunt nisi doloremque corporis officiis aut dolorum molestiae quasi ea odio earum quos nobis! Facere nisi nemo autem? Quos voluptatum, id molestiae temporibus dolorum eos, asperiores alias soluta harum dicta, facere aut perferendis fugit quae possimus eius excepturi.</p></section>
+            <section><p>MISSION</p></section>
+            <section><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum tempore quae quam. Minus minima molestias tempora nihil omnis accusamus, ab porro suscipit illo inventore nulla blanditiis nemo sequi repellendus mollitia delectus? Ratione quaerat quod adipisci ullam. Pariatur modi quia nisi.</p></section>
+        </div>
+        <section class="sidebar">
+            <section class="feature"><p>FEATURE</p></section>
+            <section class="temperature"><p>TEMPERATURE</p></section>
+            <section class="qrcode"><p>QR CODE</p></section>
+            <section class="facerecognition"><p>FACE RECOGNITION</p></section>
+        </section>
+      </div>
 </body>
+<footer>
+    <h1>CONTACT <span>TRACING</span> &copy;, 2021</h1>
+</footer>
 </html>
