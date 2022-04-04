10a88a7968865a2388047bdbce026a396d742f39
hiriyoshi <81507450+hiriyoshi@users.noreply.github.com>
2022-04-04 20:44:08 +0800

    outside_index

--- index.php
+++ index.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-        <head>
-        <title>Customer login</title>
-        <meta charset="utf-8"/>
-		<meta name="viewport" content="width=device-width"/>
-        <link rel="stylesheet" href="/assets/mymain1.css"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-          </head>
-          <body>
-            <header>
-              <ul>
-                        <li>
-                        <h1 id="titlemain" >Smart Contact Tracing</h1>
-                        </li>
-              </ul>
-            </header>
-            <main>
-            <h2>Login as Customer</h2>
-            <!-- Login as Customer -->
-            <form method="post" action="customer.html">
-                      
-                      <div class="form-group">
-                      <label>Username</label>
-                      <input type="text" name="username">
-                      </div>
-                      <div class="form-group">
-                      <label>Password</label>
-                      <input type="password" name="password">
-                      <br>
-                      <button type="submit" class="btn btn-primary" name="login_user">Login</button>
-                      </div>
-            </form>
-            <a href="testPage/index.php">sample</a>
-            <a href="sample1/sample1.php">sample2</a><br>
-            <a href="USERS_FOLDER/code/index.php">code folder</a><br>
-            
-            </main>
-            <footer>
-                      <p>Page created by Smart Contact Tracing team</p>
-            </footer>
-          </body>
+<head>
+    <link rel="stylesheet" href="index.css">
+    <title>THESIS</title>
+</head>
+<header>
+<nav class="nav">
+    <div class="logo"><a href="index.html">CONTACT <span>TRACING</span></a></div>
+    <ul class="nav_link">
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>
+    <div class="nav_button">
+        <li><button class="Signup"><a href="register.php">Sign Up</a></button></li>
+        <li><button class="Login"><a href="login.php">Log In</a></button></li>
+    </div>
+</nav>
+</header>
+<body>
+      <div class="wrapper">
+        <div class="background_message">
+            <section><p>COVID-19</p></section>
+            <section><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore, harum, iure incidunt nisi doloremque corporis officiis aut dolorum molestiae quasi ea odio earum quos nobis! Facere nisi nemo autem? Quos voluptatum, id molestiae temporibus dolorum eos, asperiores alias soluta harum dicta, facere aut perferendis fugit quae possimus eius excepturi.</p></section>
+            <section><p>MISSION</p></section>
+            <section><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum tempore quae quam. Minus minima molestias tempora nihil omnis accusamus, ab porro suscipit illo inventore nulla blanditiis nemo sequi repellendus mollitia delectus? Ratione quaerat quod adipisci ullam. Pariatur modi quia nisi.</p></section>
+        </div>
+        <section class="sidebar">
+            <section class="feature"><p>FEATURE</p></section>
+            <section class="temperature"><p>TEMPERATURE</p></section>
+            <section class="qrcode"><p>QR CODE</p></section>
+            <section class="facerecognition"><p>FACE RECOGNITION</p></section>
+        </section>
+      </div>
+</body>
+<footer>
+    <h1>CONTACT <span>TRACING</span> &copy;, 2021</h1>
+</footer>
 </html>
