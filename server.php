5e6af26537bcc53dcf97b7241762ffbc35b28654
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-05 12:07:06 +0800

    Update server.php

--- server.php
+++ server.php
@@ -111,7 +111,7 @@ if (isset($_POST['login_user'])) {
 			$_SESSION['success']  = "You are now logged in";
 			header('location: dashboard.php');
 		} else {
-			array_push($errors, "Wrong username/password combination");
+			array_push($errors, "Wrong username or password");
 		}
 	}
 }
