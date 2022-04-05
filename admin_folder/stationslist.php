62cb47cfd37962c7994e1b85dc49ae632466dec6
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-05 13:05:37 +0800

    Update stationslist.php

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -10,7 +10,7 @@
           <body>
                    <div class="sidenav">
                     <ul class="first">
-                              <li> <a  href="hadmin.html">Home</a></li>
+                              <li> <a  href="index.php">Home</a></li>
                               <li> <a href="registeredUsers.php">Users List</a></li>
                               <li> <a href="adminslist.php">Admins List</a></li>
                               <li><a class="active" href="stationslist.php">Stations List</a></li>
