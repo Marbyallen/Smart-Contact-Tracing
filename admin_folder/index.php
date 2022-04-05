f2d7fb797b9dccb4056f897e1eead52c096eb708
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-05 13:33:48 +0800

    fix

--- admin_folder/index.php
+++ admin_folder/index.php
@@ -21,7 +21,7 @@ session_start();
           <body>
                    <div class="sidenav">
                     <ul class="first">
-                              <li> <a class="active" href="hadmin.html">Home</a></li>
+                              <li> <a class="active" href="index.php">Home</a></li>
                               <li> <a href="registeredUsers.php">Users List</a></li>
                               <li> <a href="adminslist.php">Admins List</a></li>
                               <li><a href="stationslist.php">Stations List</a></li>
