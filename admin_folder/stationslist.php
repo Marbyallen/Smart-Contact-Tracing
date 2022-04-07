62115da9f07d371ecf2c387238e57e2e04667921
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-07 14:05:11 +0800

    stationslist - number of rows testing

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -80,7 +80,7 @@ session_start();
                     // include "totalResults.php";
                     ?>
               <table class="table table-bordered">
-                        <tr>
+                        <!-- <tr>
                                   <th>QR code</th>
                                   <th>First Name</th>
                                   <th>Last Name</th>
@@ -91,7 +91,7 @@ session_start();
                                   <th>Date</th>
                                   <th>Time</th>
                                   <th>Body Heat Temperature</th>
-                        </tr>
+                        </tr> -->
                         <?php
                         if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                   // collect value of input field
@@ -100,9 +100,26 @@ session_start();
                                   $QRcode = $_REQUEST['QRcode'];
                                   $date1 = $_REQUEST['date1'];
                                   $date2 = $_REQUEST['date2'];
+
+                                  echo "
+                                    <tr>
+                                    <th>QR code</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Contact No.</th>
+                                    <th>Email Address</th>
+                                    <th>Station</th>
+                                    <th>Facial recognition image</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Body Heat Temperature</th>
+                                    </tr>
+                                  ";
                                   if (!empty($Sfname)) {
                                             $result = mysqli_query($mysqli," SELECT  * FROM usersAndStations_table WHERE firstname LIKE '".$Sfname."' ");
-                                                      while($row = mysqli_fetch_array($result))
+                                            $numResults = mysqli_num_rows($result);
+                                            echo "Number of rows found: " . $numResults;
+                                            while($row = mysqli_fetch_array($result))
                                                                 {
                                                                     // station facialimg date time bodyheat_temp
                                                                 echo "<tr>";
@@ -123,7 +140,9 @@ session_start();
                                   } elseif(!empty($Slname)) {
                                             
                                             $result = mysqli_query($mysqli," SELECT  * FROM usersAndStations_table WHERE lastname = '".$Slname."' ");
-                                                      while($row = mysqli_fetch_array($result))
+                                            $numResults = mysqli_num_rows($result);
+                                            echo "Number of rows found: " . $numResults;          
+                                            while($row = mysqli_fetch_array($result))
                                                       {
                                                                 echo "<tr>";
                                                                 echo "<td>" . $row['QRcode'] . "</td>";
@@ -143,7 +162,9 @@ session_start();
 
                                  } elseif(!empty($QRcode)){
                                     $result = mysqli_query($mysqli," SELECT  * FROM usersAndStations_table WHERE QRcode = '".$QRcode."' ");
-                                                        while($row = mysqli_fetch_array($result))
+                                    $numResults = mysqli_num_rows($result);
+                                    echo "Number of rows found: " . $numResults;                    
+                                    while($row = mysqli_fetch_array($result))
                                                         {
                                                                 echo "<tr>";
                                                                 echo "<td>" . $row['QRcode'] . "</td>";
@@ -163,6 +184,8 @@ session_start();
                                 //-----------------------date range filter
                                   } elseif(!empty($date1) && !empty($date2)){
                                     $result = mysqli_query($mysqli, "SELECT * FROM `usersAndStations_table` WHERE `date` BETWEEN '$date1' AND '$date2'") or die(mysqli_error());
+                                    $numResults = mysqli_num_rows($result);
+                                    echo "Number of rows found: " . $numResults;
                                     while($row = mysqli_fetch_array($result))
                                     {
                                             echo "<tr>";
@@ -181,6 +204,8 @@ session_start();
                                     echo "</table>";
                                     mysqli_close($mysqli);
                                   } else {
+                                    $numResults = mysqli_num_rows($result);
+                                    echo "Number of rows found: " . $numResults;
                                     while($row = mysqli_fetch_array($result))
                                     { 
                                                         
