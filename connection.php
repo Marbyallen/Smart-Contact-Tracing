aa984ae5c3ac32cd45e94a54ba2907a61feb7568
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-20 17:57:17 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "mysqlBbASDWE!@DF#@$9999";
-$dbname = "thesis";
-
 // $dbhost = "localhost";
-// $dbuser = "u923368226_tracingconnect";
-// $dbpass = "Mainsct21!";
-// $dbname = "u923368226_contactTracing";
+// $dbuser = "root";
+// $dbpass = "mysqlBbASDWE!@DF#@$9999";
+// $dbname = "thesis";
+
+$dbhost = "localhost";
+$dbuser = "u923368226_tracingconnect";
+$dbpass = "Mainsct21!";
+$dbname = "u923368226_contactTracing";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
