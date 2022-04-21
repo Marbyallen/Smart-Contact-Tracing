0803e39a27decf5ba78a8e7f478f3d3e9e961a0e
marbyagbunag <m.agbunagmarby@gmail.com>
2022-04-21 12:10:06 +0800

    Update admin_functions.php

--- admin_folder/admin_functions.php
+++ admin_folder/admin_functions.php
@@ -5,18 +5,19 @@ function check_login($con)
 	if(isset($_SESSION['QRcode']))
 	{
 		$id = $_SESSION['QRcode'];
-		
 		$query = "SELECT * FROM admin_table WHERE QRcode LIKE  '".$id."' LIMIT 1";
 
 		$result = mysqli_query($con,$query);
 		if($result && mysqli_num_rows($result) > 0)
 		{
+
 			$user_data = mysqli_fetch_assoc($result);
 			return $user_data;
-		}else {
+		}
+	} else {
+        //redirect to login
         header("Location: adminlogin.php");
         die;
-		}
     }
 }
 
