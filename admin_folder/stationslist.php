aea02a5ab5f7d5adad3a5cbdd422674e88463ff5
marbyagbunag <m.agbunagmarby@gmail.com>
2022-11-30 15:15:01 +0800

    onclick table test 6

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -78,6 +78,12 @@ session_start();
                       window.print();
                       document.body.innerHTML = originalContents;
                     }
+                    //onclick row table
+                    $(document).ready(function() {
+                      $(document.body).on("click", "tr[data-href]", function() {
+                        window.location.href = this.dataset.href;
+                      });
+                    });
                   </script>
                     <?php
                       include "db_connect.php";
@@ -230,13 +236,6 @@ session_start();
                           }
                         ?>
                     </div>
-                    <script>
-                          $(document).ready(function() {
-                            $(document.body).on("click", "tr[data-href]", function() {
-                              window.location.href = this.dataset.href;
-                            });
-                          });
-                    </script>
                     
 </main>
   </body>
