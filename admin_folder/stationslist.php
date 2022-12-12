8223f82740e8d62323874f6be99226cbceaecd4d
marbyagbunag <m.agbunagmarby@gmail.com>
2022-12-12 17:31:46 +0800

    Update stationslist.php

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -124,7 +124,7 @@ session_start();
                         class_users[i].onclick = function(){
                           let text = "generate report that is near to this user?";
                           if (confirm(text) == true) {
-                            text = "You pressed OK!";
+                            location.href = "https://code-boxx.com/redirect-on-click-javascript/";
                           } else {
                             text = "You canceled!";
                           }
