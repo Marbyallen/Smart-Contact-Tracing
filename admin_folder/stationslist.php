ab609e64239daa77bad5518fa34dca947f877fcd
marbyagbunag <m.agbunagmarby@gmail.com>
2022-12-12 16:28:23 +0800

    Update stationslist.php

--- admin_folder/stationslist.php
+++ admin_folder/stationslist.php
@@ -101,7 +101,7 @@ session_start();
                         ?>    
                     </div>
                     <script>
-                    //===============================================================================
+                      //===============================================================================
                       function printDiv(printableTable){
                         var printContents = document.getElementById(printableTable).innerHTML;
                         var originalContents = document.body.innerHTML;
@@ -110,100 +110,7 @@ session_start();
                         window.print();
                         document.body.innerHTML = originalContents;
                       }
-
-                      //Auto refresh table
-                      //TODO: Xaxis should not move after reload page
-
-                      let fname = <?php echo json_encode($Sfname); ?>;
-                      let lname = <?php echo json_encode($Slname); ?>;
-                      let QRcode = <?php echo json_encode($QRcode); ?>;
-                      let date1 = <?php echo json_encode($date1); ?>;
-                      let date2 = <?php echo json_encode($date2); ?>;
-                      // let toggle = <?php echo json_encode($toggle); ?>;
-
-                      function setCookie(user_input, value, days) {
-                      var expires = "";
-                        if (days) {
-                            var date = new Date();
-                            date.setTime(date.getTime() + (days*24*60*60*1000));
-                            expires = "; expires=" + date.toUTCString();
-                        }
-                      }
-                      function getCookie(user_input) {
-                          var nameEQ = user_input + "=";
-                          var ca = document.cookie.split(';');
-                          for(var i=0;i < ca.length;i++) {
-                              var c = ca[i];
-                              while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                              if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                          }
-                          return null;
-                      }
-
-                      setCookie("first_name", fname,30);
-                      setCookie("last_name", lname,30);
-                      setCookie("user_qrcode", QRcode,30);
-                      setCookie("user_date1", date1,30);
-                      setCookie("user_date2", date2,30);
-                      // setCookie("toggle_switch", toggle,30);
-                      var userFirstname=getCookie("first_name");
-                      var userFirstname=getCookie("last_name");
-                      var userFirstname=getCookie("user_qrcode");
-                      var userFirstname=getCookie("user_date1");
-                      var userFirstname=getCookie("user_date2");
-                      // var userFirstname=getCookie("toggle_switch");
-
-                      //fix scroll
-                      document.addEventListener("DOMContentLoaded", function(event) { 
-                        var scrollpos = localStorage.getItem('scrollpos');
-                        if (scrollpos) window.scrollTo(0, scrollpos);
-                      }); 
-
-                      window.onbeforeunload = function(e) {
-                          localStorage.setItem('scrollpos', window.scrollY, window.scrollX);
-                      };
-                      // Phase 2 refresh
-                      // REFRESH LOOP~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                      setTimeout(() => {
-                          document.location.reload(true);
-                          console.log("setTimeout is called");
-                      }, 5000);
-                      
-                      
-
-                      //Auto generate report after clicking button========================================================================
-                      //test 3
-                      //
-                      var class_users = document.getElementsByClassName("redirectPage");
-                      //pass array from php array to JS(object)
-                      var obj = <?php echo json_encode($temparray); ?>;
-                      //object to array
-                      const toArray = Object.entries(obj); 
-                      console.log("typeof toArray: ", toArray);
-                      //
-                      var userArray = [];
-                      
-                      for (var i=0; i < class_users.length; i++) {
-                        class_users[i].onclick = function(){
-                              alert("generate report that is near to this user?");
-                              // var secondDimension = 2;
-                              // var dateLoc = 7;
-                              // var timeLoc = 8;
-                              
-                              // userArray.push(toArray[i][secondDimension][dateLoc], toArray[i][secondDimension][timeLoc]);
-                              // console.log(typeof userArray);
-                              // console.info("userArray: ", userArray);
-                              // console.log(typeof obj);
-                              // console.info("my Obj: ", obj);
-                              // console.log(typeof class_users);
-                              // console.info("my class_users: ", class_users);
-                          }
-                      }
-
-
-                      
-
-                  </script>
+                    </script>
 </main>
   </body>
 </html>
