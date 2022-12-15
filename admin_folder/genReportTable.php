d5b905a34e4884ed4d4ce779c3ff3941e464ca86
marbyagbunag <m.agbunagmarby@gmail.com>
2022-12-15 16:44:29 +0800

    Update genReportTable.php

--- admin_folder/genReportTable.php
+++ admin_folder/genReportTable.php
@@ -31,36 +31,61 @@
 
 
     //method 2 test
-        echo "
-            <tr>
-            <th>QR code</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Contact No.</th>
-            <th>Email Address</th>
-            <th>Station</th>
-            <th>Facial recognition image</th>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Body Heat Temperature</th>
-            </tr>
-        ";
-        array_push($temparray,$row);                  
-        echo "<tr class = " . $class . ">";
-        echo "<td>" . $row['QRcode'] . "</td>";
-        echo "<td>" . $row['firstname'] . "</td>";
-        echo "<td>" . $row['lastname'] . "</td>";
-        echo "<td>" . $row['contactno'] . "</td>";
-        echo "<td>" . $row['email'] . "</td>";
-        echo "<td>" . $row['station'] . "</td>";
-        echo "<td>" . $row['facialimg'] . "</td>";
-        echo "<td>" . $row['date'] . "</td>";
-        echo "<td>" . $row['time'] . "</td>";
-        echo "<td>" . $row['bodyheat_temp'] . "</td>";
-        echo "</tr>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // $station = $_REQUEST['station'];
+            // $date1 = $_REQUEST['date1'];
+
+            $time2 = $_POST["time2"];
+            $time3 = $_POST["time3"];
+            echo "time1 value: " . $time1;
+            echo "<br>";
+            echo "time2 value: " . $time2;
+            echo "<br>";
+            echo "time3 value: " . $time3;
+            echo "<br>";
+            $station = $_POST["station"];
+            $date1 = $_POST["date1"];
+
+            $class = "redirectPage";
+            $temparray = array();
+
+            echo "
+                <tr>
+                <th>QR code</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Contact No.</th>
+                <th>Email Address</th>
+                <th>Station</th>
+                <th>Facial recognition image</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Body Heat Temperature</th>
+                </tr>
+            ";
             
-        echo "</table>";
-        mysqli_close($mysqli);
+            $numResults = mysqli_num_rows($result);
+            echo "Number of rows found: " . $numResults;
+            while($row = mysqli_fetch_array($result))
+            { 
+                array_push($temparray,$row);                  
+                echo "<tr class = " . $class . ">";
+                echo "<td>" . $row['QRcode'] . "</td>";
+                echo "<td>" . $row['firstname'] . "</td>";
+                echo "<td>" . $row['lastname'] . "</td>";
+                echo "<td>" . $row['contactno'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['station'] . "</td>";
+                echo "<td>" . $row['facialimg'] . "</td>";
+                echo "<td>" . $row['date'] . "</td>";
+                echo "<td>" . $row['time'] . "</td>";
+                echo "<td>" . $row['bodyheat_temp'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            mysqli_close($mysqli);
+            
+        }
     //
 
     // if ($_SERVER["REQUEST_METHOD"] == "POST") {
